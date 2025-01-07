a323dea052ffc2ec2f9d8bb15a1f90172cba9749
Michael Harfenist <hrharfenist@gmail.com>
2025-01-07 15:37:22 -0500

    upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="header grey">
+    <header className="header grid grey">
       <img src="Logo.svg" alt="Logo" className="logo" />
       <Nav></Nav>
     </header>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,21 @@
 const Main = () => {
-  return <main></main>;
+  return (
+    <main>
+      <div className="grid green height-holder">
+        <div className="half flex flex-col">
+          <h1>Little Lemon</h1>
+          <h3>Chicago</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="half flex center-flex">
+          <img src="restaurant.jpg" className="center-img" />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
